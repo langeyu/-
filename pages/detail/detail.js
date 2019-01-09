077ca60cec2363f9780fcd9ec7e2b88ce624e145
langeyu <46509625+langeyu@users.noreply.github.com>
2019-01-09 10:44:46 +0800

    Add files via upload

---
+++ pages/detail/detail.js
@@ -0,0 +1,17 @@
+var app = getApp();
+
+Page({
+  data: {
+    imgUrls: [
+      '/images/thing1.jpg',
+      '/images/thing2.jpg',
+      '/images/thing3.jpg'
+    ],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 3000,
+    duration: 1200,
+    iscollect: true
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '/images/swiper01.jpg',
+      '/images/swiper02.jpg',
+      '/images/swiper03.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+  }
+
+  
+})
